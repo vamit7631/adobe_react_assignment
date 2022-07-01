7296625ae4a53fd5b8956be02ab25208feef6f0f
vamit7631 <vamit7631@gmail.com>
2022-07-01 13:55:19 +0530

    update hash browser code

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import './index.css';
 import App from './App';
 import {
-  BrowserRouter as Router
+  HashRouter as Router
 } from 'react-router-dom';
 import {Provider} from "react-redux";
 import store from './redux/store';
