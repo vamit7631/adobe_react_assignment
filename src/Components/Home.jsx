9423ae722388cb8c45dbdfbed465dafef5a909d5
vamit7631 <vamit7631@gmail.com>
2022-07-13 16:27:54 +0530

    update home page

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -15,7 +15,7 @@ const Home = () => {
                   
           </div>
           <div class="banner-img-grid aem-GridColumn aem-GridColumn--default--6 aem-GridColumn--phone--12">
-          <div class="banner-img"><img src={'./images/banner.png'} /></div>
+          <div class="banner-img"><img src={'./images/banner.png'} alt="product-img" /></div>
           </div>
             
         </div>
